1698693fb204d92aaa6e0120701d3386fef8140d
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-01-16 16:52:38 +0100

    fixtures: add coverage for TestApp::*_state_* methods

--- src/fixtures.rs
+++ src/fixtures.rs
@@ -6,7 +6,7 @@ use bevy_pbr::{MaterialPlugin, StandardMaterial};
 use bevy_render::{mesh::MeshPlugin, render_resource::Shader, texture::ImagePlugin};
 use bevy_state::state::{FreelyMutableState, NextState, State, States};
 use bevy_window::{ExitCondition, WindowPlugin};
-use rstest::{fixture, rstest};
+use rstest::fixture;
 
 #[derive(Debug, Deref, DerefMut)]
 pub struct TestApp(pub App);
@@ -66,14 +66,92 @@ pub fn test_app<P>(
     app
 }
 
-#[rstest]
-fn test_minimal_app_is_created(mut minimal_test_app: TestApp) {
-    minimal_test_app.update();
-    drop(minimal_test_app);
-}
+#[cfg(test)]
+mod tests {
+    use bevy_state::{
+        app::{AppExtStates, StatesPlugin},
+        state::{NextState, States},
+    };
 
-#[rstest]
-fn test_test_app_is_created(mut test_app: TestApp) {
-    test_app.update();
-    drop(test_app);
+    use rstest::{fixture, rstest};
+    use speculoos::{assert_that, asserting, option::OptionAssertions, string::StrAssertions};
+
+    use crate::{minimal_test_app, test_app, TestApp};
+
+    #[rstest]
+    fn test_minimal_app_is_created(mut minimal_test_app: TestApp) {
+        minimal_test_app.update();
+        drop(minimal_test_app);
+    }
+
+    #[rstest]
+    fn test_test_app_is_created(mut test_app: TestApp) {
+        test_app.update();
+        drop(test_app);
+    }
+
+    #[derive(States, Debug, Copy, Clone, PartialEq, Eq, Hash)]
+    enum MyState {
+        First,
+        Second,
+    }
+
+    #[fixture]
+    fn states_app(
+        #[from(minimal_test_app)]
+        #[with(StatesPlugin)]
+        app: TestApp,
+    ) -> TestApp {
+        app
+    }
+
+    #[rstest]
+    fn test_app_get_state(#[from(states_app)] mut app: TestApp) {
+        asserting!("TestApp::get_state() before MyState exists")
+            .that(&app.get_state::<MyState>())
+            .is_none();
+
+        app.insert_state(MyState::First);
+
+        asserting!("TestApp::get_state() when MyState exists")
+            .that(&app.get_state::<MyState>())
+            .is_some()
+            .is_equal_to(&MyState::First);
+    }
+
+    #[rstest]
+    fn test_app_get_next_state(#[from(states_app)] mut app: TestApp) {
+        asserting!("TestApp::get_next_state() before MyState exists")
+            .that(&app.get_next_state::<MyState>())
+            .is_none();
+
+        app.insert_state(MyState::First);
+
+        let next_state = app.get_next_state::<MyState>();
+
+        asserting!("TestApp::get_next_state() when MyState exists")
+            .that(&next_state)
+            .is_some();
+        assert_that!(format!("{:?}", next_state.unwrap()))
+            .is_equal_to(format!("{:?}", NextState::<MyState>::Unchanged));
+    }
+
+    #[rstest]
+    fn test_app_set_next_state(#[from(states_app)] mut app: TestApp) {
+        asserting!("TestApp::set_next_state() before MyState exists")
+            .that(&app.set_next_state(MyState::First))
+            .is_none();
+
+        app.insert_state(MyState::First);
+        asserting!("TestApp::set_next_state() before MyState exists")
+            .that(&app.set_next_state(MyState::Second))
+            .is_some();
+
+        let next_state = app.get_next_state::<MyState>();
+        asserting!("TestApp::get_next_state() after set_next_state()")
+            .that(&next_state)
+            .is_some();
+        assert_that!(format!("{:?}", next_state.unwrap()))
+            .contains(format!("{:?}", MyState::Second));
+    }
 }
