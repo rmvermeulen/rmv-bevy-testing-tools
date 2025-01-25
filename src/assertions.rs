a37f8ae20a54ba3f3180a3ffc2e53c9d7554c7d7
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-01-25 15:37:02 +0100

    add speculoos assertions

---
+++ src/assertions.rs
@@ -0,0 +1,23 @@
+use speculoos::{AssertionFailure, Spec};
+
+pub trait IsContainedIn<T>
+where
+    T: PartialEq + std::fmt::Debug,
+{
+    fn is_contained_in(&mut self, collection: &[T]);
+}
+
+impl<T> IsContainedIn<T> for Spec<'_, T>
+where
+    T: PartialEq + std::fmt::Debug,
+{
+    fn is_contained_in(&mut self, collection: &[T]) {
+        let subject = self.subject;
+        if !collection.contains(subject) {
+            AssertionFailure::from_spec(self)
+                .with_expected(format!("<{subject:?}> to be contained in <{collection:?}>"))
+                .with_actual("but wasn't.".into())
+                .fail();
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(coverage_attribute)]
 
 pub mod app;
+#[cfg(feature = "speculoos")]
+pub mod assertions;
 pub mod events;
 #[cfg(any(test, feature = "rstest"))]
 pub mod fixtures;
@@ -17,8 +19,27 @@ macro_rules! set_snapshot_suffix {
 }
 
 pub mod prelude {
-
-    #[cfg(any(test, feature = "rstest"))]
+    #[cfg(feature = "rstest")]
     pub use super::fixtures::*;
+    #[cfg(feature = "insta")]
+    pub use super::set_snapshot_suffix;
     pub use super::{app::*, events::*, traits::*};
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "rstest")]
+    use rstest::rstest;
+
+    use super::prelude::*;
+
+    #[cfg(feature = "rstest")]
+    #[rstest]
+    fn can_access_everything(mut test_app: TestApp) {
+        #[derive(bevy_ecs::event::Event, Default, Debug, Copy, Clone)]
+        struct MyEvent;
+        test_app
+            .collect_events::<MyEvent>()
+            .send_event_default::<MyEvent>();
+    }
 }
