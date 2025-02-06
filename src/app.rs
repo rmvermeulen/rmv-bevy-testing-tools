fba4f9d911a2d486e3d5ab2684584adcc0186975
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-02-06 19:52:46 +0100

    app: order impl blocks

--- src/app.rs
+++ src/app.rs
@@ -14,6 +14,20 @@ use crate::{
 #[derive(Debug, Deref, DerefMut)]
 pub struct TestApp(pub App);
 
+impl TestApp {
+    pub fn get_state<S: States>(&self) -> Option<&S> {
+        self.world().get_resource::<State<S>>().map(|s| s.get())
+    }
+    pub fn get_next_state<S: FreelyMutableState>(&self) -> Option<&NextState<S>> {
+        self.world().get_resource::<NextState<S>>()
+    }
+    pub fn set_next_state<S: FreelyMutableState>(&mut self, next: S) -> Option<()> {
+        self.world_mut()
+            .get_resource_mut::<NextState<S>>()
+            .map(|mut s| s.set(next))
+    }
+}
+
 impl Drop for TestApp {
     fn drop(&mut self) {
         self.world_mut().send_event(AppExit::Success);
@@ -28,20 +42,6 @@ impl ManageState for TestApp {
         self.world().get_resource::<NextState<S>>()
     }
     fn set_next_state<S: FreelyMutableState>(&mut self, next: S) -> Option<()> {
-        self.world_mut()
-            .get_resource_mut::<NextState<S>>()
-            .map(|mut s| s.set(next))
-    }
-}
-
-impl TestApp {
-    pub fn get_state<S: States>(&self) -> Option<&S> {
-        self.world().get_resource::<State<S>>().map(|s| s.get())
-    }
-    pub fn get_next_state<S: FreelyMutableState>(&self) -> Option<&NextState<S>> {
-        self.world().get_resource::<NextState<S>>()
-    }
-    pub fn set_next_state<S: FreelyMutableState>(&mut self, next: S) -> Option<()> {
         self.world_mut()
             .get_resource_mut::<NextState<S>>()
             .map(|mut s| s.set(next))
