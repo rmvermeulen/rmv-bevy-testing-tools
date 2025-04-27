ce8cdbdab343a12a1c9e015dda313decdee04a59
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-04-27 22:04:20 +0200

    feature-combinations-tests.ps1: pass arg to check and/or test

--- feature-combination-tests.ps1
+++ feature-combination-tests.ps1
@@ -1,11 +1,26 @@
 #!/usr/bin/env pwsh
 
+# features: insta, rstest, speculoos, iter_tools, trait_manage_state
+
+param(
+    [switch] $Check,
+    [switch] $Test
+)
+
+if (!$Check -and !$Test)
+{
+    Write-Host "Pass '-Check' and/or '-Test' as an argument"
+    exit 1
+}
+
 $feature_groups = @( 
+    # 1
     @("insta"),
     @("rstest"),
     @("speculoos"),
     @("iter_tools"),
     @("trait_manage_state"),
+    # 2
     @("insta", "rstest"),
     @("insta", "speculoos"),
     @("insta", "iter_tools"),
@@ -15,36 +30,50 @@ $feature_groups = @(
     @("rstest", "trait_manage_state"),
     @("speculoos", "iter_tools"),
     @("speculoos", "trait_manage_state"),
+    @("iter_tools", "trait_manage_state"),
+    # 3
     @("insta", "rstest", "speculoos"),
     @("insta", "rstest", "iter_tools"),
     @("insta", "rstest", "trait_manage_state"),
     @("insta", "speculoos", "iter_tools"),
     @("insta", "speculoos", "trait_manage_state"),
+    @("insta", "iter_tools", "trait_manage_state"),
     @("rstest", "speculoos", "iter_tools"),
     @("rstest", "speculoos", "trait_manage_state"),
     @("rstest", "iter_tools", "trait_manage_state"),
+    # 4
     @("insta", "rstest", "speculoos", "iter_tools"),
     @("insta", "rstest", "speculoos", "trait_manage_state"),
     @("insta", "rstest", "iter_tools", "trait_manage_state"),
     @("insta", "speculoos", "iter_tools", "trait_manage_state"),
     @("rstest", "speculoos", "iter_tools", "trait_manage_state"),
+    # 5
     @("insta", "rstest", "speculoos", "iter_tools", "trait_manage_state")
 )
 
 foreach ($items in $feature_groups)
 {
     $features = $items -join ","
-    Write-Host "Running check+test for features: '$features'"
-    cargo check --no-default-features --features $features 2>&1 1>$null
-    if (! $?)
+    if ($Check)
     {
-        Write-Error "check failed for '$features'"
-        exit 1
+        Write-Host "Running check for features: '$features'"
+        $output = cargo check --no-default-features --features $features 2>&1
+        if (! $?)
+        {
+            Write-Output $output
+            Write-Error "check failed for '$features'"
+            exit 1
+        }
     }
-    cargo test --no-default-features --features $features 2>&1 1>$null
-    if (! $?)
+    if ($Test)
     {
-        Write-Error "test failed for '$features'"
-        exit 1
+        Write-Host "Running test for features: '$features'"
+        $output = cargo test --no-default-features --features $features 2>&1
+        if (! $?)
+        {
+            Write-Output $output
+            Write-Error "test failed for '$features'"
+            exit 1
+        }
     }
 }
