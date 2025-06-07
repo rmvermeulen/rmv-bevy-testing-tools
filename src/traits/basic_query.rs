6afcb7c7ea2dba3811fcc21de749a933eba4d7e9
Rasmus <rasmus_vermeulen@tutanota.com>
2025-06-08 00:35:23 +0200

    basic_query: simplify bounds

--- src/traits/basic_query.rs
+++ src/traits/basic_query.rs
@@ -3,17 +3,12 @@ use bevy_ecs::query::QueryData;
 use crate::prelude::TestApp;
 
 pub trait BasicQuery {
-    fn query_any<'a, Q, C>(&mut self) -> bool
-    where
-        Q: QueryData<Item<'a> = C>;
+    fn query_any<Q: QueryData>(&mut self) -> bool;
 }
 
 #[doc = include_str!("./basic_query.md")]
 impl BasicQuery for TestApp {
-    fn query_any<'a, Q, C>(&mut self) -> bool
-    where
-        Q: QueryData<Item<'a> = C>,
-    {
+    fn query_any<Q: QueryData>(&mut self) -> bool {
         let mut q = self.world_mut().query::<Q>();
         q.iter(self.world()).next().is_some()
     }
